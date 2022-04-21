733a8dffefab63ae486abf0ce30ee7c24590d350
simran <95364621+Simran8800@users.noreply.github.com>
2022-04-21 14:26:35 +0530

    Add files via upload

---
+++ file-question3.py
@@ -0,0 +1,102 @@
+# banks_list = ["Kotak", "HDFC", "RBL", "SBI", "Bank of Baroda"]
+# f=open("file-question3.txt",'w')
+# a=f.write("Kotak\nHDFC\nRBL\nSBI\nBank of Baroda")
+# print(a)
+
+
+banks_list = ["Kotak", "HDFC", "RBL", "SBI", "Bank of Baroda"]
+f=open("file-question3.txt",mode='r')
+a=f.write(banks_list)
+# for line in f:
+print(a)
+f.close()
+
+
+
+# a=[33,-4,56,67,44,-4,23]
+# i=0
+# while i<len(a):
+#     if a[i]>50:
+#         print("hi")
+#     elif a[i]<50 and a[i]>0:
+#         print("no")
+#     elif a[i]<0:
+#         print("hello")
+#     else:
+#         print("aa")
+#     i=i+1
+
+
+# name=input("enetr:")
+# if name=="ram":
+#     print("ji")
+# if name=="simi":
+#     print("df")
+# elif name=="ishu":
+#     print("dfghj")
+# else:
+#     if name=="ishi":
+#         print("oo")
+#     elif name=="ishu":
+#         print("xcn")
+#     else:
+#         print("oop")
+# if name=="ishi":
+#     print("qwe")
+# else:
+#     print("mcv")
+# if name=="ram":
+#     print("dfgh")
+# elif name=="aayu":
+#     print("asd")
+#     if name=="ishu":
+#         print("df")
+#     else:
+#         print("set")
+# elif name=="aayu":
+#     print("sdd")
+# else:
+#     if name=="aayu":
+#         print("sf")
+#     else: 
+#         print("asdd")
+#     if name=="ishu":
+#         print("sdfghj")
+#     else:
+#         print("1234567io")
+    
+
+# a=input("enter the name")   
+# if a[1]=="s":
+#     print("yes")
+# else:
+#     print("no")
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ fileq2.py
@@ -0,0 +1,9 @@
+from itertools import count
+
+
+f=open("people1-exercise.txt","r")
+count=0
+for i in f:
+    count=count+1
+print(count)
+f.close()
